acada304a5e30075db2da1244f06dab0ee9f908b
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2019-01-08 20:25:43 -0500

    Added beginning work on .c files

--- tests/DigitalIn.c
+++ tests/DigitalIn.c
@@ -1,3 +1,13 @@
 #include "DigitalIn.h"
 
-int main(void) {}
+void digitalin_init_config(pin_name p, pull_type t, output_mode o, speed s) {
+    
+}
+
+void digitalin_init(pin_name p) {
+    
+}
+
+GPIO_PinState digitalin_read(pin_name p) {
+    
+}
--- tests/DigitalOut.c
+++ tests/DigitalOut.c
@@ -1,3 +1,23 @@
 #include "DigitalOut.h"
 
-int main(void) {}
+void digitalout_init_config(pin_name p, pull_type t, output_mode o, speed s) {
+    __HAL_RCC_GPIOx_CLK_ENABLE();
+    
+    GPIO_InitTypeDef pin_structure;
+    pin_structure.Pin = p.pin_number;
+    pin_structure.Mode = o;
+    pin_structure.Pull = t;
+    pin_structure.Speed = s
+    
+    HAL_GPIO_Init(p.bank, );
+}
+
+void digitalout_init(pin_name p) {
+    
+}
+void digitalout_write(pin_name p, GPIO_PinState s) {
+    
+}
+GPIO_PinState digitalout_read(pin_name p) {
+    
+}
