413b0620ee41548d31303eaa2f51331233ddeaff
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2019-01-15 20:52:58 -0500

    Working on fixing my code to work in new build arrangement

---
+++ API/c/Inc/digital_out.h
@@ -0,0 +1,13 @@
+#include "mtrain.h"
+
+typedef enum { PUSH_PULL = GPIO_MODE_OUTPUT_PP, OPEN_DRAIN = GPIO_MODE_OUTPUT_OD } pin_mode;
+
+typedef enum { LOW = GPIO_SPEED_FREQ_LOW, MEDIUM = GPIO_SPEED_FREQ_MEDIUM, HIGH = GPIO_SPEED_FREQ_HIGH, VERY_HIGH = GPIO_SPEED_FREQ_VERY_HIGH } pin_speed;
+
+void digitalout_init(pin_name pin);
+void digitalout_init_ex(pin_name pin, pull_type type, pin_mode mode, pin_speed speed);
+void digitalout_deinit(pin_name pin);
+
+void digitalout_write(pin_name pin, GPIO_PinState state);
+void digitalout_toggle(pin_name pin);
+GPIO_PinState digitalout_read(pin_name pin);
---
+++ API/c/Src/digital_out.c
@@ -0,0 +1,37 @@
+#include "digital_out.h"
+
+void digitalout_init_ex(pin_name pin, pull_type pull, pin_mode mode, pin_speed speed) {
+    GPIO_InitTypeDef pin_structure;
+    pin_structure.Pin = pin.number;
+    pin_structure.Mode = mode;
+    pin_structure.Pull = pull;
+    pin_structure.Speed = speed;
+    
+    HAL_GPIO_Init(pin.bank, &pin_structure);
+}
+
+void digitalout_init(pin_name pin) {
+    GPIO_InitTypeDef pin_structure;
+    pin_structure.Pin = pin.number;
+    pin_structure.Mode = OPEN_DRAIN;
+    pin_structure.Pull = PULL_UP;
+    pin_structure.Speed = GPIO_SPEED_FREQ_LOW;
+    
+    HAL_GPIO_Init(pin.bank, &pin_structure);
+}
+
+void digitalout_deinit(pin_name pin) {
+    HAL_GPIO_DeInit(pin.bank, pin.number);
+}
+
+void digitalout_write(pin_name pin, GPIO_PinState state) {
+    HAL_GPIO_WritePin(pin.bank, pin.number, state);
+}
+
+void digitalout_toggle(pin_name pin) {
+    return HAL_GPIO_TogglePin(pin.bank, pin.number);
+}
+
+GPIO_PinState digitalout_read(pin_name pin) {
+    return HAL_GPIO_ReadPin(pin.bank, pin.number);
+}
