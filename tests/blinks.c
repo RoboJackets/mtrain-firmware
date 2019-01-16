b01dcbd490f36d6765c78dd35768a5f8ff088fc5
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2019-01-15 20:17:07 -0500

    Added digitalOut tests in blinks

--- tests/blinks.c
+++ tests/blinks.c
@@ -1,39 +1,59 @@
 #include "mtrain.h"
 #include "digital_out.h"
-//#include "digital_in.h"
 
 int main(void)
 {
-  digitalout_init(Pin3);
-  //digitalin_init(Pin4);
+  pin_name pin1 = (pin_name) {LED_PORT, LED1_PIN};
+  pin_name pin2 = (pin_name) {LED_PORT, LED2_PIN};
+  pin_name pin3 = (pin_name) {LED_PORT, LED3_PIN};
+  pin_name pin4 = (pin_name) {LED_PORT, LED4_PIN};
   
-  GPIO_InitTypeDef GPIO_InitStruct;
+  // Basic Init Testing
+  digitalout_init(pin1);
+  digitalout_init(pin2);
+  digitalout_init(pin3);
+  digitalout_init(pin4);
   
-  uint32_t leds[4] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN};
-
-  GPIO_InitStruct.Pin = leds[0] | leds[1] | leds[2] | leds[3];
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
-
-  int id = 0;
-  int forward = 1;
+  // Deinit Testing
+  digitalout_deinit(pin1);
+  digitalout_deinit(pin2);
+  digitalout_deinit(pin3);
+  digitalout_deinit(pin4);
+  
+  // Complex Init Testing
+  digitalout_init_config(pin1, PULL_DOWN, PUSH_PULL, LOW);
+  digitalout_init_config(pin2, PULL_DOWN, PUSH_PULL, LOW);
+  digitalout_init_config(pin3, PULL_DOWN, PUSH_PULL, LOW);
+  digitalout_init_config(pin4, PULL_DOWN, PUSH_PULL, LOW);
   while (1) {
-    HAL_Delay(100);
-    HAL_GPIO_WritePin(LED_PORT, leds[id], GPIO_PIN_RESET);
-    if (id == 1) {
-      forward = 0;
-    } else if (id == 0) {
-      forward = 1;
-    }
-
-    if (forward) {
-      id++;
-    } else {
-      id--;
-    }
-
-    HAL_GPIO_WritePin(LED_PORT, leds[id], GPIO_PIN_SET);
+    // Normal Write Testing
+    // digitalout_write(pin1, GPIO_PIN_RESET);
+    // digitalout_write(pin2, GPIO_PIN_RESET);
+    // digitalout_write(pin3, GPIO_PIN_RESET);
+    // digitalout_write(pin4, GPIO_PIN_RESET);
+    // HAL_Delay(250);
+    // digitalout_write(pin1, GPIO_PIN_SET);
+    // digitalout_write(pin2, GPIO_PIN_SET);
+    // digitalout_write(pin3, GPIO_PIN_SET);
+    // digitalout_write(pin4, GPIO_PIN_SET);
+    // HAL_Delay(250);
+    
+    // Toggle Testing
+    // digitalout_toggle(pin1);
+    // digitalout_toggle(pin2);
+    // digitalout_toggle(pin3);
+    // digitalout_toggle(pin4);
+    // HAL_Delay(250);
+    
+    // Read Testing
+    digitalout_init(Pin36);
+    digitalout_init(Pin35);
+    digitalout_init(Pin34);
+    digitalout_init(Pin33);
+    digitalout_write(pin1, digitalout_read(Pin36));
+    digitalout_write(pin2, digitalout_read(Pin35));
+    digitalout_write(pin3, digitalout_read(Pin34));
+    digitalout_write(pin4, digitalout_read(Pin33));
+    HAL_Delay(250);
   }
 }
