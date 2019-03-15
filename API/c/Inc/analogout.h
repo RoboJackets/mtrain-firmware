7c39adf280519dd979b6620f80e6c0930e1058b3
james2213 <jstrock3@gatech.edu>
2019-03-15 00:03:31 -0400

    h file complete

--- API/c/Inc/analogout.h
+++ API/c/Inc/analogout.h
@@ -1,5 +1,4 @@
-#include <mtrain.h>
-
+#include "pin_defs.h"
 #ifndef __DAC_H
 #define __DAC_H
 
@@ -7,7 +6,7 @@ void analogout_init(pin_name pin);
 void analogout_deinit(pin_name pin);
 
 void analogout_write(pin_name pin, float value);
-void analogout_read(pin_naame pin);
+void analogout_read(pin_name pin);
 
 #endif /* __MAIN_H */
 
