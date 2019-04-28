8b95f5d28a3e8790c1b6efb317256fbc71cecb16
james2213 <jstrock3@gatech.edu>
2019-04-28 14:14:05 -0400

    test for dac pin

--- tests/c/dac.c
+++ tests/c/dac.c
@@ -1,8 +1,29 @@
 #include "mtrain.h"
 
 int main(void) {
-    analog_out_init(PIN33);
+    analogout_init(PIN33);
 
-    analogout_write(PIN33, 0.5);
+    float value = 0;
+    int forward = 0;
+
+    while (1) {
+        analogout_write(PIN33,value);
+        analogout_read(PIN33);
+        HAL_Delay(1);
+        if (forward ==0){
+            value += 0.01;
+        } 
+        else {
+            value -= -0.01;
+        }
+        if (value>= 1) {
+            forward == 1;
+
+        }
+        else if (value <= 0) {
+            forward =0;
+        }
+         
+
+    }
 }
-
