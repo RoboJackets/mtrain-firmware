514d691365daecf67b080ff77c69cc00e04486d6
CollinAvidano <collin.avidano@gmail.com>
2020-07-04 19:12:49 -0400

    slowly changing build system to use new model

---
+++ BSP/config/bsp_defs.h
@@ -0,0 +1,40 @@
+/**
+  ******************************************************************************
+  * @file           : bsp_defs.h
+  * @brief          : Global Board Configuration Definitions
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 RoboJackets.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by RoboJackets under Apache License
+  * 2.0; You may not use this file except in compliance with the License. You 
+  * may obtain a copy of the License at: 
+  *                    https://www.apache.org/licenses/LICENSE-2.0
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BSP_DEFS_H
+#define __BSP_DEFS_H
+
+
+/* Board Specific Global Definitions -----------------------------------------*/
+
+/* Architecture Definitions */
+
+// #define STM32F4xx
+// #define STM32F7xx
+
+/* Hardware Modules to include */
+
+// #define USE_USB_FS
+// #define USE_USB_HS
+// #define USE_QSPI
+
+/* Operating System Modules */
+
+// #define USE_RTOS
+
+#endif /* _BSP_DEFS_H */
